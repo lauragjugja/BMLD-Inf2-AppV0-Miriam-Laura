828783369e0a7c4aea0d88a891c8143137771ed3
Laura Gjugja <gjugjlau@students.zhaw.ch>
2026-03-11 09:19:15 +0100

    Session state

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import pandas as pd  # --- NEW CODE: add pandas to the imports ---
 import streamlit as st
 
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+
+# --- NEW CODE: initialize empty data frame if not already present ---
+if 'data_df' not in st.session_state:
+    st.session_state['data_df'] = pd.DataFrame()
+# --- END OF NEW CODE ---
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/quad_Formel_berechnen.py", title="Mitternachtsformel Rechner", icon=":material/info:")
